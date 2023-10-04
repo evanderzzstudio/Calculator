968fed8d1dd130ee787e84f9f500f88a0cd72d41
evanderzzstudio <evanderstudio@gmail.com>
2023-10-04 13:18:14 +0700

    changes

--- addStyles.js
+++ addStyles.js
@@ -5,16 +5,15 @@ bodyClass.split(" ").forEach((cls) => bdy.classList.add(cls));
 const calcBodyClass =
     "max-w-lg rounded overflow-hidden shadow-2xl bg-pink-700 bg-opacity-75 bg-gradient-to-tr from-gray-800 flex"
 const calcBody = document.querySelector("calcBody");
-calcBodyClass.split(" ").forEach(cls) => calcBody.classList.add(cls);
-
+calcBodyClass.split(" ").forEach((cls) => calcBody.classList.add(cls));
 
 const outputClass =
-    "flex flex-col 2-3/4 h-12 justify-center items-end m-1 bg-gray-300 text-gray-900 font-semibold px-2 border border-black hover:border-gray-500 rounded"
+    "flex flex-col 2-3/4 h-12 justify-center items-end m-1 bg-gray-300 text-gray-900 font-semibold px-2 border border-black hover:border-gray-500 rounded";
 const output = document.querySelector(".output");
 outputClass.split(" ").forEach((cls) => output.classList.add(cls));
 
 const previousNumberClass =
-    "flex max-w-lg h-1/3 pb-5 pt-1 text-gray-600 text-xs"
+    "flex max-w-lg h-1/3 pb-5 pt-1 text-gray-600 text-xs";
 const previousNumber = document.querySelector(".previousNumber");
 previousNumberClass
     .split(" ")
--- main.js
+++ main.js
@@ -2,14 +2,20 @@ let currentNum = "";
 let previousNum = "";
 let operator = "";
 
-const currentDisplayNumber = document.querySelector("currentNumber");
-const previousDisplayNumber = document.querySelector("previousNumber");
+const currentDisplayNumber = document.querySelector(".currentNumber");
+const previousDisplayNumber = document.querySelector(".previousNumber");
 
 const equal = document.querySelector(".equal");
-
+equal.addEventListener("click", () => {
+    if (currentNum != "" && previousNum != "") {
+        calculate()
+    }
+});
 const decimal = document.querySelector(".decimal");
 
 const clear = document.querySelector(".clear");
+clear.addEventListener("click", clearCalculator)
+
 
 const numberButtons = document.querySelectorAll(".number");
 
@@ -22,5 +28,67 @@ numberButtons.forEach(btn => {
 });
 
 function handleNumber(number) {
-    console.log(number);
+    if (currentNum.length <= 11) {
+        currentNum += number;
+        currentDisplayNumber.textContent = currentNum;
+    }
+}
+
+operator.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        handleOperator(e.target.textContent);
+    });
+})
+
+function handleOperator(op) {
+    operator = op;
+    previousNumber = currentNum;
+    previousDisplayNumber.textContent = previousNum + " " + operator;
+    currentNum = "";
+    currentDisplayNumber.textContent = "";
+}
+
+function calculate() {
+    previousNum = Number(previousNum);
+    currentNum = Number(currentNum);
+
+    if (operator === "+") {
+        previousNum += currentNum;
+    } else if (operator === "-") {
+        previousNum -= currentNum;
+    } else if (operator === "x") {
+        previousNum *= currentNum;
+    } else if (operator === '/') {
+        if (currentNum <= 0) {
+            previousNum = "Error";
+            displayResult()
+            return;
+        }
+        previousNum /= currentNum;
+    }
+    previousNum = roundNumber(previousNum);
+    previousNum = previousNum.toString();
+    displayResult();
+}
+
+function roundNumber(num) {
+    return Math.round(num * 100000) / 100000;
+}
+
+function displayResult() {
+    previousDisplayNumber.textContent = "";
+    operator = "";
+    if (previousNum.length <= 11) {
+        currentDisplayNumber.textContent = previousNum;
+    } else {
+        currentDisplayNumber.textContent = previousNum.slice(0, 11) + "...";
+    }
+}
+
+function clearCalculator() {
+    currentNum = "";
+    previousNum = "";
+    operator = "";
+    currentDisplayNumber.textContent = "0";
+    previousDisplayNumber.textContent = "";
 }
