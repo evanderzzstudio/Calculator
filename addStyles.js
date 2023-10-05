0323bb44a76a0e7ae1f8eaaff4e22044f9b15921
evanderzzstudio <evanderstudio@gmail.com>
2023-10-05 10:41:00 +0700

    fix bug in output and button

--- addStyles.js
+++ addStyles.js
@@ -4,11 +4,11 @@ bodyClass.split(" ").forEach((cls) => bdy.classList.add(cls));
 
 const calcBodyClass =
     "max-w-lg rounded overflow-hidden shadow-2xl bg-pink-700 bg-opacity-75 bg-gradient-to-tr from-gray-800 flex"
-const calcBody = document.querySelector("calcBody");
+const calcBody = document.querySelector(".calcBody");
 calcBodyClass.split(" ").forEach((cls) => calcBody.classList.add(cls));
 
 const outputClass =
-    "flex flex-col 2-3/4 h-12 justify-center items-end m-1 bg-gray-300 text-gray-900 font-semibold px-2 border border-black hover:border-gray-500 rounded";
+    "flex flex-col w-3/4 h-12 justify-center items-end m-1 bg-gray-300 text-gray-900 font-semibold px-2 border border-black hover:border-gray-500 rounded";
 const output = document.querySelector(".output");
 outputClass.split(" ").forEach((cls) => output.classList.add(cls));
 
@@ -20,7 +20,7 @@ previousNumberClass
     .forEach((cls) => previousNumber.classList.add(cls));
 
 
-const currentNumberClass = "flex max - w - lg h - 2 / 3 pb - 2 text - xl";
+const currentNumberClass = "flex max-w-lg h-2/3 pb-2 text-xl";
 const currentNumber = document.querySelector(".currentNumber");
 currentNumberClass
     .split(" ")
@@ -32,26 +32,26 @@ buttons.forEach((btn) => {
 });
 
 function addClasses(button) {
-    const btnNumberClass = "hover: bg-green-500 border-green-500";
-    const btnClearClass = "hover: bg-orang-500 border-red-500";
-    const btnOpClass = "hover: bg-blue-500 border-blue-500";
-    const btnEqClass = "hover: bg-red-500 border-red-500";
-    const btnDecClass = "hover: bg-yellow-500 border-yellow-500";
+    const btnNumberClass = "hover:bg-green-500 border-green-500";
+    const btnClearClass = "hover:bg-orang-500 border-red-500";
+    const btnOpClass = "hover:bg-blue-500 border-blue-500";
+    const btnEqClass = "hover:bg-red-500 border-red-500";
+    const btnDecClass = "hover:bg-yellow-500 border-yellow-500";
 
     if (button.classList.value === "number") {
-        btnNumberClass.split(" ").forEach(cls => button.classList.add(cls));
+        btnNumberClass.split(" ").forEach((cls) => button.classList.add(cls));
     } else if (button.classList.value === "operator") {
-        btnOpClass.split(" ").forEach(cls => button.classList.add(cls));
+        btnOpClass.split(" ").forEach((cls) => button.classList.add(cls));
     } else if (button.classList.value === "clear") {
-        btnClearClass.split(" ").forEach(cls => button.classList.add(cls));
-    } else if (button.classList.value === "equal") {
         btnClearClass.split(" ").forEach((cls) => button.classList.add(cls));
+    } else if (button.classList.value === "equal") {
+        btnEqClass.split(" ").forEach((cls) => button.classList.add(cls));
     } else if (button.classList.value === "decimal") {
         btnDecClass.split(" ").forEach((cls) => button.classList.add(cls));
     }
 
 
     const baseClass =
-        "numberflex w-12 h-12 justify-center items-center m-1 font-bold py-2 px-2 border hover:border-transparent rounded text-white border-2 text-2xl  ";
+        "number flex w-12 h-12 justify-center items-center m-1 font-bold py-2 px-2 border hover:border-transparent rounded text-white border-2 text-2xl";
     baseClass.split(" ").forEach((cls) => button.classList.add(cls));
 }   
